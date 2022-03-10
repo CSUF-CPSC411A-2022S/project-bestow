8267d818f2113f0a61eb980640fc0c4f81a6c096
Gavin Gray <graygavin111997@gmail.com>
2022-03-09 20:02:08 -0800

    Added Private Vals to class

--- app/src/main/java/com/example/testproject/UserProfile.kt
+++ app/src/main/java/com/example/testproject/UserProfile.kt
@@ -2,5 +2,11 @@ package com.example.testproject
 
 class UserProfile (var item: String, var pictureID: String, var description: String,
                    var scores: String) {
-    
+        private val userItem = item;
+        private val userPictureID = pictureID;
+        private val userDescription = description;
+        private val userScores = scores;
+
+
+
 }
