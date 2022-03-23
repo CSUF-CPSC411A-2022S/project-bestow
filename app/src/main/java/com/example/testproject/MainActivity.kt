1bee1014b8b4c8e25ebf2aab0126c825e18b0d96
Gavin Gray <graygavin111997@gmail.com>
2022-03-22 20:22:04 -0700

    Created a home screen and added a navigation menu

--- app/src/main/java/com/example/testproject/MainActivity.kt
+++ app/src/main/java/com/example/testproject/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.testproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.testproject.fragments.AccountFragment
+import com.example.testproject.fragments.HomeFragment
+import com.example.testproject.fragments.PostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val accountFragment = AccountFragment()
+        val postFragment = PostFragment()
+
     }
 }
