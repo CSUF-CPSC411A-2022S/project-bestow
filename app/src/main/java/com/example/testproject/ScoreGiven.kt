cfe32cca96d1dcb988355af08f120681e53dda64
ranf2000 <60449466+ranf2000@users.noreply.github.com>
2022-03-24 11:22:32 -0700

    ScoreGiven is change to actually work.

---
+++ app/src/main/java/com/example/testproject/ScoreGiven.kt
@@ -0,0 +1,11 @@
+package com.example.testproject
+
+class GivingScore(var score: String)
+{
+
+    fun current(item: String, item_rating: Int)
+    {
+        var rating_score: Int = 500
+        score += rating_score
+    }
+}
