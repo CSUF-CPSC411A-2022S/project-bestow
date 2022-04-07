d70e859d8b2b3241d0ffb10321f0268ae85a9b1f
Randall Frye <rcfrye18@csu.fullerton.edu>
2022-04-07 13:51:29 -0700

    A view model has been created

---
+++ app/src/main/java/com/example/testproject/ViewModel_Project.kt
@@ -0,0 +1,7 @@
+package com.example.testproject
+
+import androidx.lifecycle.ViewModel
+
+class ViewModel_Project: ViewModel() {
+
+}
--- app/src/main/java/com/example/testproject/screen_givingscore.kt
+++ app/src/main/java/com/example/testproject/screen_givingscore.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +21,8 @@ class screen_givingscore : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private val viewModel: ViewModel_Project by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
